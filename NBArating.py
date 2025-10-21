69e3471f43f0975555f01dfdc6fecdc7114a819e
Victor Ganson <vicganson@gmail.com>
2025-10-21 18:11:48 -0400

    Implement rivalry, marketability, and competitiveness functions
    Added functions for rivalry, marketability, and competitiveness.

--- NBArating.py
+++ NBArating.py
@@ -1,1 +1,14 @@
+def rivalry(home, away):
+    if (home, away) in rivalries or (away, home) in rivalries
+        return 5
+    else return 0
 
+def marketability(home, away):
+    return team_marketability.get(home, 5) + team_marketability.get(away, 5)
+    
+def competitiveness(home, away):
+    homeRecord = records.get(home).split("-")
+    awayRecord = records.get(away).split("-")
+    
+
+def gameImportance(home, away):
