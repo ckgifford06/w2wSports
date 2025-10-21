cda3de274a0bc8ba568b231d3bbfa56eb3d903a4
Victor Ganson <vicganson@gmail.com>
2025-10-21 18:10:07 -0400

    Refactor score calculation by removing functions
    Removed rivalry and marketability functions from score calculation.

--- app.py
+++ app.py
@@ -66,22 +66,7 @@ rivalries = [
 ]
 
 def calculate_score(home, away):
-    return rivalry(home, away) + marketability(home, away)
-
-def rivalry(home, away):
-    if (home, away) in rivalries or (away, home) in rivalries
-        return 5
-    else return 0
-
-def marketability(home, away):
-    return team_marketability.get(home, 5) + team_marketability.get(away, 5)
-    
-def competitiveness(home, away):
-    homeRecord = records.get(home).split("-")
-    awayRecord = records.get(away).split("-")
-    
-
-def gameImportance(home, away):
+    return
 
 @app.route('/')
 def index():
