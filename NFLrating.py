aa2bf6d8b1c98f98c4975448fb6a47c69170a23e
Victor Ganson <vicganson@gmail.com>
2025-10-22 14:23:01 -0400

    Refactor NFL rating calculations and data structures

--- NFLrating.py
+++ NFLrating.py
@@ -1,3 +1,7 @@
+import requests
+url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+data = requests.get(url).json()
+
 season_length = 17
 playoffs = False
 
@@ -6,20 +10,32 @@ team_marketability = {
     "CHI_NFL": 9, "CIN_NFL": 8, "CLE_NFL": 7, "DAL_NFL": 10, "DEN_NFL": 8,
     "DET_NFL": 7.5, "GB_NFL": 9, "HOU_NFL": 6.5, "IND_NFL": 6, "JAX_NFL": 6,
     "KC_NFL": 10, "LV_NFL": 9, "LAC_NFL": 6.5, "LAR_NFL": 8, "MIA_NFL": 8,
-    "MIN_NFL": 7.5, "NE_NFL": 10, "NO_NFL": 8, "NYG_NFL": 9, "NYJ_NFL": 8.5,
+    "MIN_NFL": 7.5, "BUF_NFL": 10, "NO_NFL": 8, "NYG_NFL": 9, "NYJ_NFL": 8.5,
     "PHI_NFL": 9.5, "PIT_NFL": 9, "SF_NFL": 10, "SEA_NFL": 8, "TB_NFL": 8,
     "TEN_NFL": 6.5, "WAS_NFL": 7
 }
-rivalries = [
+rivalries = {
     
-]
+}
+team_division = {
+    "BUF_NFL": "AFC East", "MIA_NFL": "AFC East", "NE_NFL": "AFC East", "NYJ_NFL": "AFC East",
+    "BAL_NFL": "AFC North", "CIN_NFL": "AFC North", "CLE_NFL": "AFC North", "PIT_NFL": "AFC North",
+    "HOU_NFL": "AFC South", "IND_NFL": "AFC South", "JAX_NFL": "AFC South", "TEN_NFL": "AFC South",
+    "DEN_NFL": "AFC West", "KC_NFL": "AFC West", "LV_NFL": "AFC West", "LAC_NFL": "AFC West",
+    "DAL_NFL": "NFC East", "NYG_NFL": "NFC East", "PHI_NFL": "NFC East", "WAS_NFL": "NFC East",
+    "CHI_NFL": "NFC North", "DET_NFL": "NFC North", "GB_NFL": "NFC North", "MIN_NFL": "NFC North",
+    "ATL_NFL": "NFC South", "CAR_NFL": "NFC South", "NO_NFL": "NFC South", "TB_NFL": "NFC South",
+    "ARI_NFL": "NFC West", "LAR_NFL": "NFC West", "SF_NFL": "NFC West", "SEA_NFL": "NFC West"
+}
 
 def rivalry(home, away):
-    #Could factor in divisions
+    rating = 0
+    if team_division.get(home) == team_vision.get(away):
+        rating += 5
     for t1, t2, r in rivalries:
         if (t1 == home and t2 == away) or (t2 == home and t1 == away):
-            return r
-    else: return 0
+            rating += r
+    return rating
 
 def marketability(home, away):
     return team_marketability.get(home, 5) + team_marketability.get(away, 5)
