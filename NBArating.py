cee01857ae2ca981146987f1725e414a13e2f7af
Charles Gifford <ckgifford06@gmail.com>
2025-10-22 21:07:15 -0400

    Add importance variable to NBArating.py

--- NBArating.py
+++ NBArating.py
@@ -4,6 +4,7 @@ data = requests.get(url).json()
 
 season_length = 82
 playoffs = False
+importance = 0
 
 team_marketability = {
     "BOS_NBA": 9, "BKN_NBA": 7, "NY_NBA": 10, "PHI_NBA": 8, "TOR_NBA": 6,
