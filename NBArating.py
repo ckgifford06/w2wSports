efdec78fdc4fd9bed0774dcb6a5d1ed82e3c7df5
Charles Gifford <ckgifford06@gmail.com>
2025-10-23 09:52:02 -0400

    Round quality rating to three decimal places
    Round the quality rating to three decimal places.

--- NBArating.py
+++ NBArating.py
@@ -89,7 +89,7 @@ def qualityOfPlay(home, away):
     gamesPlayed = float(homeRecord[0] + homeRecord[1] + awayRecord[0] + awayRecord[1])
     if gamesPlayed == 0:
         return 0
-    quality = (combinedWins / gamesPlayed)*10
+    quality = round(((combinedWins / gamesPlayed)*10), 3)
     return quality
     
 def gameImportance(home, away):
