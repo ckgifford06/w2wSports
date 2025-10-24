7ac7968275506fe3710f0513745a9a5188b0bd22
Victor Ganson <vicganson@gmail.com>
2025-10-24 12:54:36 -0400

    Add functions for rivalry, marketability, and more

--- MLBrating.py
+++ MLBrating.py
@@ -36,3 +36,61 @@ def buildSeeds():
 
 def calculateScore(home, away):
     return 0
+
+
+def rivalry(home,away):
+    for t1, t2, r in rivalries:
+        if (t1 == home and t2 == away) or (t2 == home and t1 == away):
+            return r
+    else: return 0
+
+def marketability(home, away):
+    return team_marketability.get(home, 5) + team_marketability.get(away, 5)
+
+def competitiveness(home, away):
+    records = buildRecords()
+    homeRecord = list(map(int, records.get(home, "0-0").split("-")))
+    awayRecord = list(map(int, records.get(away, "0-0").split("-")))
+    try:
+        winDiff = abs(homeRecord[0] - awayRecord[0])
+    except ValueError:
+        winDiff = 0
+    compRank = (20 - winDiff) / 5
+    return compRank
+
+def qualityOfPlay(home, away):
+    records = buildRecords()
+    homeRecord = list(map(int, records.get(home, "0-0").split("-")))
+    awayRecord = list(map(int, records.get(away, "0-0").split("-")))
+    combinedWins = float(homeRecord[0] + awayRecord[0])
+    gamesPlayed = float(homeRecord[0] + homeRecord[1] + awayRecord[0] + awayRecord[1])
+    if gamesPlayed == 0:
+        return 0
+    quality = round(((combinedWins / gamesPlayed)*10), 3)
+    return quality
+    
+def gameImportance(home, away):
+    importance = 0
+    seeds = buildSeeds()
+    records = buildRecords()
+    homeRecord = list(map(int, records.get(home, "0-0").split("-")))
+    awayRecord = list(map(int, records.get(away, "0-0").split("-")))
+    homeGamesPlayed = int(homeRecord[0]) + int(homeRecord[1])
+    awayGamesPlayed = int(awayRecord[0]) + int(awayRecord[1])
+    home_seed = seeds.get(home)
+    away_seed = seeds.get(away)
+    gamesLeft = 82 - max(homeGamesPlayed, awayGamesPlayed)
+    if playoffs:
+        pass
+    else:
+        if gamesLeft <= 50:
+            importance += 1
+        if gamesLeft <= 30:
+            importance += 1
+        if gamesLeft <= 20:
+            importance += 1
+        if home_seed is not None and 6 <  home_seed < 11:
+            importance += 3
+        if away_seed is not None and 6 < away_seed < 11:
+            importance += 3
+    return importance
