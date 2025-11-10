ad2a60fee53d2fe34704acd97318a0a2fc2631d3
Victor Ganson <vicganson@gmail.com>
2025-11-10 14:00:30 -0500

    Update MLBrating.py

--- MLBrating.py
+++ MLBrating.py
@@ -3,7 +3,7 @@ url = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
 data = requests.get(url).json()
 
 season_length = 162
-playoffs = True
+playoffs = False
 
 
 team_marketability = {
