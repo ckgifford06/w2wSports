30e727419ede34741e7b57bdfa9e7ba39b1e825c
Charles Gifford <ckgifford06@gmail.com>
2025-12-19 13:59:23 -0500

    Adjust quality calculation factor from 10 to 7

--- CFBrating.py
+++ CFBrating.py
@@ -131,7 +131,7 @@ def qualityOfPlay(home, away):
     gamesPlayed = float(homeRecord[0] + homeRecord[1] + awayRecord[0] + awayRecord[1])
     if gamesPlayed == 0:
         return 0
-    quality = round(((combinedWins / gamesPlayed)*10), 3)
+    quality = round(((combinedWins / gamesPlayed)*7), 3)
     if homeRank and homeRank <= 25:
         quality += (25 - homeRank) // 2
     if awayRank and awayRank <= 25:
