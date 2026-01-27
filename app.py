b1ba62bd370e50a80b3b8ce05c65b963bd135b34
Charles Gifford <ckgifford06@gmail.com>
2026-01-27 12:23:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -616,5 +616,5 @@ def verify():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
